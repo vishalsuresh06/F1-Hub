e537b44a67c37b1d99bb303402f6ca4cc1786c40
Vishal Suresh <vishalsuresh@Vishals-MBP.attlocal.net>
2025-08-21 06:56:10 -0500

    Removed unnecesary session code

--- auth.ts
+++ auth.ts
@@ -3,21 +3,4 @@ import GitHub from 'next-auth/providers/github'
 
 export const {auth, handlers, signIn, signOut} = NextAuth({
     providers: [GitHub],
-    session: {
-        strategy: "jwt",
-    },
-    callbacks: {
-        async session({ session, token }) {
-            if (token.sub && session.user) {
-                session.user.id = token.sub;
-            }
-            return session;
-        },
-        async jwt({ token, user }) {
-            if (user) {
-                token.sub = user.id;
-            }
-            return token;
-        },
-    },
 })
